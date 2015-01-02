150ce72d6beb14d2d5df1f86a874580f5f395dd2
dirg <dirg.zeus@gmail.com>
2015-01-02 23:46:21 +0700

    clean public folder

--- public/assets/js/advanced-datatable/examples/server_side/scripts/id_jsonp.php
+++
@@ -1,9 +0,0 @@
-<br />
-<b>Warning</b>:  include(/home/andcor33/alvarez.is/datatables/mysql.php) [<a href='function.include'>function.include</a>]: failed to open stream: No such file or directory in <b>/home/andcor33/alvarez.is/demo/gridgum/dashgum/assets/js/advanced-datatable/examples/server_side/scripts/id_jsonp.php</b> on line <b>24</b><br />
-<br />
-<b>Warning</b>:  include() [<a href='function.include'>function.include</a>]: Failed opening '/home/andcor33/alvarez.is/datatables/mysql.php' for inclusion (include_path='.:/usr/local/lib/php:/usr/local/php5/lib/pear') in <b>/home/andcor33/alvarez.is/demo/gridgum/dashgum/assets/js/advanced-datatable/examples/server_side/scripts/id_jsonp.php</b> on line <b>24</b><br />
-<br />
-<b>Warning</b>:  mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: Can't connect to local MySQL server through socket '/No-MySQL-hostname-was-specified' (2) in <b>/home/andcor33/alvarez.is/demo/gridgum/dashgum/assets/js/advanced-datatable/examples/server_side/scripts/id_jsonp.php</b> on line <b>45</b><br />
-<br />
-<b>Warning</b>:  Cannot modify header information - headers already sent by (output started at /home/andcor33/alvarez.is/demo/gridgum/dashgum/assets/js/advanced-datatable/examples/server_side/scripts/id_jsonp.php:24) in <b>/home/andcor33/alvarez.is/demo/gridgum/dashgum/assets/js/advanced-datatable/examples/server_side/scripts/id_jsonp.php</b> on line <b>37</b><br />
-Could not open connection to server
